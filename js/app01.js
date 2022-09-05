34a9bbb3116c21d1348bf72c3d685c1c728d03e7
Fabio Avalos <avalos2106@gmail.com>
2022-09-05 19:38:49 -0300

    Finalizo ejercicio 01 en app01.js

---
+++ js/app01.js
@@ -0,0 +1,6 @@
+//DESARROLLO DEL EJERCICIO 01
+const MONTHS=["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+console.log("Los meses del año son:")
+for(let i=0; i<12; i++){
+  console.log(`${MONTHS[i]}`);
+}
