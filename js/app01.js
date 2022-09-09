50a18410106a8baf57e04993a2125faf3708b725
Fabio Avalos <avalos2106@gmail.com>
2022-09-09 20:56:57 -0300

    uso funcion join para mostrar con alert en forma de lista

--- js/app01.js
+++ js/app01.js
@@ -4,3 +4,5 @@ console.log("Los meses del año son:")
 for(let i=0; i<12; i++){
   console.log(`${MONTHS[i]}`);
 }
+//generando un string de un array y separado por un salto de linea
+alert(MONTHS.join("\n"));
---
+++ js/app02.js
@@ -0,0 +1,5 @@
+//DESARROLLO DEL EJERCICIO 02
+const DAY=["lu","ma","mi","ju","vi","sa","do"];
+const NEW_DAY= DAY.fill("fe1",0,3);
+console.log("DAY:"+DAY);
+console.log("NEW_DAY:"+NEW_DAY);
